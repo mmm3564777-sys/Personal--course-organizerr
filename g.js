cc8e4bca86196f21c8fe88400c32764025bad3b4
mmm3564777-sys <mmm3564777@gmail.com>
2026-04-24 23:03:35 +0300

    Update g.js

--- g.js
+++ g.js
@@ -1,19 +1,10 @@
-/**
- * Search Functionality
- * Filters game cards based on user input
- */
 function searchGames() {
-    // Get input value and convert to lowercase
     let input = document.getElementById('searchInput').value.toLowerCase();
-    
-    // Get all game card elements
     let cards = document.getElementsByClassName('game-card');
 
     for (let i = 0; i < cards.length; i++) {
-        // Get the title inside the current card
         let title = cards[i].getElementsByTagName('h3')[0].innerText.toLowerCase();
         
-        // Show card if title matches, otherwise hide it
         if (title.includes(input)) {
             cards[i].style.display = "block";
         } else {
@@ -22,11 +13,6 @@ function searchGames() {
     }
 }
 
-/**
- * Launch Game Function
- * Simulates starting a game
- */
 function launchGame(name) {
-    alert("Initializing Connection: Loading " + name + "...");
-    console.log("System: Starting game engine for " + name);
+    alert("Launching " + name + "... Prepare for battle!");
 }
